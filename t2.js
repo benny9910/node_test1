7168823bdee4db1eba4ee4e7583bb5e4cb60af8f
Benny <Benny@Win10-Z97>
2023-04-23 18:16:34 +0800

    init

---
+++ t2.js
@@ -0,0 +1,18 @@
+    var curdate="2021-06-27";
+    var epgdate="2021-06-28";
+
+    var week = new Date(epgdate).getDay();
+
+    
+    if (week==0) {
+        week = 7;
+    }
+
+
+    if (new Date(curdate).getDay()==0) {
+        week = week + 7;
+    }
+
+	console.log(new Date(curdate).getDay());
+	console.log(new Date(epgdate).getDay());
+	console.log(week);
